e47733e8146359204d3da51a70dc59dd6b3f07c7
Syed Zubair Ali Shah <154551822+syedzubairalishah@users.noreply.github.com>
2024-10-30 07:32:42 -0700

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen text-center mt-10">
+      <h1 className="font-bold text-4xl m-10">Hello World!</h1>
+      <h1 className="font-bold text-4xl m-10">Next Js</h1>
+      <h1 className="font-bold text-4xl">Syed Zubair Ali Shah</h1>
+    </div>
+  );
+}
